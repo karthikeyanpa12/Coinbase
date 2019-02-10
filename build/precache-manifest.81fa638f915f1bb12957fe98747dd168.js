85085e97ce51776459b82dc52c0ea24ca919625f
karthi <karthi@karthis-mbp.home>
2019-02-10 01:34:48 +0000

    deploy build added

---
+++ build/precache-manifest.81fa638f915f1bb12957fe98747dd168.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3ad76c9314a42499c47a",
+    "url": "/static/js/main.3ad76c93.chunk.js"
+  },
+  {
+    "revision": "87e3446ebff6fc3bce65",
+    "url": "/static/js/1.87e3446e.chunk.js"
+  },
+  {
+    "revision": "3ad76c9314a42499c47a",
+    "url": "/static/css/main.e17d5e73.chunk.css"
+  },
+  {
+    "revision": "397f0a1465ad152ee4d45ab58ef725ea",
+    "url": "/index.html"
+  }
+];
